75d95b345a7c4af115cfad8cafe63ecd14a8da07
soyoonJ <sytkfkd96@gmail.com>
2024-10-06 04:37:12 +0900

    refactor: AppContent, App 통합 (AppContent 삭제)

--- packages/assignment/src/App.tsx
+++ packages/assignment/src/App.tsx
@@ -4,31 +4,24 @@ import { useMemo } from "./@lib";
 import { NotificationProvider, ThemeProvider, UserProvider } from "./providers";
 import { ComplexForm, Header, ItemList } from "./components";
 
-const AppContent: React.FC = () => {
+const App: React.FC = () => {
 	const [items] = useState(useMemo(() => generateItems(10000), []));
 
-	return (
-		<>
-			<Header />
-			<div className="container mx-auto px-4 py-8">
-				<div className="flex flex-col md:flex-row">
-					<div className="w-full md:w-1/2 md:pr-4">
-						<ItemList items={items} />
-					</div>
-					<div className="w-full md:w-1/2 md:pl-4">
-						<ComplexForm />
-					</div>
-				</div>
-			</div>
-		</>
-	);
-};
-const App: React.FC = () => {
 	return (
 		<ThemeProvider>
 			<NotificationProvider>
 				<UserProvider>
-					<AppContent />
+					<Header />
+					<div className="container mx-auto px-4 py-8">
+						<div className="flex flex-col md:flex-row">
+							<div className="w-full md:w-1/2 md:pr-4">
+								<ItemList items={items} />
+							</div>
+							<div className="w-full md:w-1/2 md:pl-4">
+								<ComplexForm />
+							</div>
+						</div>
+					</div>
 				</UserProvider>
 			</NotificationProvider>
 		</ThemeProvider>
