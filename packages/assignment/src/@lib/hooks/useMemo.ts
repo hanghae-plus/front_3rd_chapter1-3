943ba133103f56eb36a0617048d70717572f54af
soyoonJ <sytkfkd96@gmail.com>
2024-10-06 00:10:35 +0900

    fix: useMemo.ts prevDeps.current 값 없는 케이스 추가

--- packages/assignment/src/@lib/hooks/useMemo.ts
+++ packages/assignment/src/@lib/hooks/useMemo.ts
@@ -14,7 +14,7 @@ export function useMemo<T>(factory: () => T, deps: DependencyList, equals = shal
 
 	// 2. 현재 의존성과 이전 의존성 비교
 	// 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
-	if (!equals(prevDeps.current, deps)) {
+	if (!prevDeps.current || !equals(prevDeps.current, deps)) {
 		prevResult.current = factory();
 		prevDeps.current = deps;
 	}
