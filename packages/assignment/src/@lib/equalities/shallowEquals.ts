ab5fe5c4454151395e84617c8a955297dd2faa9f
최아람 <aarmakrr@hanmail.net>
2024-10-05 19:34:49 +0900

    [feat]: shallowEquals 구현

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,4 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+	if (typeof objA !== typeof objB) return false;
+
+	if (objA === objB) return true;
+
+	if (Array.isArray(objA) && Array.isArray(objB)) {
+		return objA.filter((item) => objB.includes(item)).length === objA.length;
+	}
+
+	if (typeof objA === "object") {
+		const intersection = [];
+		const keys = Object.keys(objB);
+
+		for (const [key, value] of Object.entries(objA)) {
+			if (keys.includes(key)) {
+				if (objB[key] === value) {
+					intersection.push(key);
+				}
+			}
+		}
+
+		return intersection.length === keys.length;
+	}
+
+	return false;
 }
