d7062cc1b8b089515448b6e659f7818213d7bf25
soyoonJ <sytkfkd96@gmail.com>
2024-10-05 22:57:55 +0900

    feat: memo.ts 구현

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -2,11 +2,21 @@ import { shallowEquals } from "../equalities";
 import { ComponentType } from "react";
 
 export function memo<P extends object>(
-  Component: ComponentType<P>,
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  equals = shallowEquals
+	Component: ComponentType<P>,
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	equals = shallowEquals
 ) {
-  return Component;
+	let prevProps = null;
+	let memoizedComponent = null;
+
+	return function (props) {
+		if (prevProps === null || !equals(prevProps, props)) {
+			memoizedComponent = Component(props);
+		}
+
+		prevProps = props;
+		return memoizedComponent;
+	};
 }
