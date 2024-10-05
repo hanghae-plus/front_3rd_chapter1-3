d2f5185d90f9b497e2c0284d8d8cae832a08d633
soyoonJ <sytkfkd96@gmail.com>
2024-10-05 22:31:23 +0900

    feat: deepEquals.ts 구현

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function deepEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+	if (objA === undefined || objA === null || typeof objA !== "object") return objA === objB;
+
+	const keysA = Object.keys(objA);
+	const keysB = Object.keys(objB);
+
+	if (keysA.length !== keysB.length) return false;
+
+	for (const key of keysA) {
+		if (typeof objA[key] === "object" && typeof objB[key] === "object") {
+			if (!deepEquals(objA[key], objB[key])) return false;
+		} else if (objA[key] !== objB[key]) return false;
+	}
+
+	return true;
 }
