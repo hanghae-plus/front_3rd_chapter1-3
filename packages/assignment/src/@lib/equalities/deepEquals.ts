939244f7b9b890504410a18ff6644b761af2d562
terry.hwang <terry.hwang@h2ohospitality.io>
2024-10-05 20:20:21 +0900

    깊은 비교 함수 로직 추가

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function deepEquals(objA: any, objB: any): boolean {
+  if (typeof objA !== typeof objB) return false;
+
+  if (typeof objA === "object" && !!objA && !!objB) {
+    return Object.keys(objA).every((key) => deepEquals(objA[key], objB[key]));
+  }
+
   return objA === objB;
 }
