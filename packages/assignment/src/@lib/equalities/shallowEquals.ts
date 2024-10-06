b3e66251f19b8d167f9e48f3c4833262da1ddee9
이동아 <christar99@naver.com>
2024-10-06 18:40:24 +0900

    shallowEquals 테스트 완성

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,4 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    return objA.every((item, index) => item === objB[index]);
+  }
+  if (typeof objA === "object" && typeof objB === "object") {
+    for (const i in objA) {
+      if (objA[i] !== objB[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   return objA === objB;
 }
