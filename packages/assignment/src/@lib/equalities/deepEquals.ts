1f0da749ca0d0b305130ca4141fc96dc6eda83c2
yujin-sof <yujinsoft1111@gmail.com>
2024-10-07 08:54:39 +0900

    equalities 만들기 완료

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,45 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function deepEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+  
+  if(objA === objB) return true;
+
+  if(typeof objA !== typeof objB) return false;
+
+  // 1. 기본 타입이거나 null인 경우 처리
+  if(objA == null || objB == null) return false;
+
+  
+  if(typeof objA !== 'object' && typeof objB !== 'object') {
+    return objA === objB;
+  }
+
+  // 2. 둘 다 객체인 경우:
+  //    - 배열인지 확인
+  if(Array.isArray(objA) && Array.isArray(objB)) {
+    if(objA.length !== objB.length) return false;
+    for(let i=0; i<objA.length; i++) {
+      // 같으면(true) !와 만나서 false가 되어 if문이 실행하지 않고 true 반환
+      // 다르면(false) !와 만나서 true가 되어 if문 실행하여 flase 반환
+      if(!deepEquals(objA[i], objB[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  //    - 객체의 키 개수가 다른 경우 처리
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if(keysA.length !== keysB.length) return false;
+
+  //    - 재귀적으로 각 속성에 대해 deepEquals 호출
+  for(let key of keysA) {
+    if(!objB.hasOwnProperty(key) || !deepEquals((objA)[key], (objB)[key])){
+      return false;
+    }
+  }
+
+  return true;
+
 }
--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,5 +1,5 @@
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, useRef, useState } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
@@ -8,5 +8,29 @@ export function memo<P extends object>(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   equals = shallowEquals
 ) {
-  return Component;
+  // 2. 메모이제이션된 컴포넌트 생성
+  return function memoizedComponent(props: P) {
+
+      // 1. 이전 props를 저장할 ref 생성
+      const prevPropsRef = useRef<P | null>(null);
+      const shouldRender = useRef(true)
+
+      // 3. equals 함수를 사용하여 props 비교
+      if (prevPropsRef !== null && equals(prevPropsRef.current, props)) {
+        shouldRender.current = false;
+      } else {
+        // 4. props가 변경된 경우에만 새로운 렌더링 수행
+        shouldRender.current = true;
+      }
+
+      prevPropsRef.current = props;
+
+      // 리렌더링 여부에 따라 컴포넌트를 렌더링
+      if (!shouldRender.current && prevPropsRef.current !== null) {
+        return null; // 리렌더링을 막음
+      }
+
+      // return React.createElement(Component, props);
+    }
+  
 }
