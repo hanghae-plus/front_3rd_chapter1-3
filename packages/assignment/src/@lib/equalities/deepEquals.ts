9bf9da7320782cc86046629153ef232e24bf10ea
남균 <gyuni9604@naver.com>
2024-10-07 00:50:05 +0900

    ✨ feat : deepEquals 구현 테스트 통과

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,41 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function deepEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+  if (objA === null || objB === null) {
+    return objA === objB
+  }
+
+  if (typeof objA === "object" && typeof objB === "object") {
+    if (Array.isArray(objA) && Array.isArray(objB)) {
+      if (objA.length !== objB.length) {
+        return false
+      }
+
+      for (let i = 0; objA.length > i; i++) {
+        if (JSON.stringify(objA[i]) !== JSON.stringify(objB[i])) {
+          return false
+        }
+      }
+      return true
+    }
+
+    const keysA = Object.keys(objA)
+    const keysB = Object.keys(objB)
+
+    if (keysA.length !== keysB.length) {
+      return false
+    }
+
+    for (let key of keysA) {
+      if (
+        !objB.hasOwnProperty(key) ||
+        JSON.stringify(objA[key]) !== JSON.stringify(objB[key])
+      ) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  return objA === objB
 }
