170d0e5a3638b0f14ec94ad49c14a9faa4b51d06
terry.hwang <terry.hwang@h2ohospitality.io>
2024-10-06 22:57:50 +0900

    useCallBack refactor

--- packages/assignment/src/@lib/hooks/useCallback.ts
+++ packages/assignment/src/@lib/hooks/useCallback.ts
@@ -3,15 +3,8 @@ import { useMemo } from "./useMemo";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-export function useCallback<T extends (...args: unknown[]) => unknown>(
-  factory: T,
-  deps: DependencyList
-): T {
+export function useCallback<T>(factory: T, deps: DependencyList): T {
   // 직접 작성한 useMemo를 통해서 만들어보세요.
-  const callBack = useMemo(() => {
-    return (...args: unknown[]) => {
-      return factory(...args);
-    };
-  }, deps);
-  return callBack as T;
+
+  return useMemo(() => factory, deps);
 }
