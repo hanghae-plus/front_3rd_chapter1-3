71cd59ce61af8649588e3108582ba67a0652fdf2
정원 <jwha94@gmail.com>
2024-10-06 20:20:37 +0900

    feat: shallowEquals 추가

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,4 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+  if (objA === objB) return true;
+
+  if (objA == null || objB == null) return false;
+
+  if (typeof objA !== 'object' || typeof objB !== 'object') {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) return false;
+
+  return Object.entries(objA).every(
+    ([key, value]) =>
+      Object.hasOwnProperty.call(objB, key) && objB[key] === value
+  );
 }
