c80ea998ccda8ea7f0e000b664b5aae6f64be971
최아람 <aarmakrr@hanmail.net>
2024-10-06 20:22:58 +0900

    [refactor]: 컴포넌트랑 프로바이더 분리

---
+++ packages/assignment/src/providers/theme/ThemeProvider.tsx
@@ -0,0 +1,37 @@
+import React, { useState, createContext, useContext } from "react";
+import { useCallback } from "../../@lib/hooks/useCallback";
+import { useMemo } from "../../@lib/hooks/useMemo";
+
+interface ThemeContextType {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const useThemeContext = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useThemeContext must be used within an AppProvider");
+  }
+  return context;
+};
+
+export const ThemeProvider = ({ children }: React.ReactNode) => {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = useCallback(() => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  }, []);
+
+  const themeValue: ThemeContextType = useMemo(
+    () => ({
+      theme,
+      toggleTheme,
+    }),
+    [theme, toggleTheme]
+  );
+
+  return (
+    <ThemeContext.Provider value={themeValue}>{children}</ThemeContext.Provider>
+  );
+};
---
+++ packages/assignment/src/providers/user/UserProvider.tsx
@@ -0,0 +1,51 @@
+import React, { useState, createContext, useContext } from "react";
+import { useCallback } from "../../@lib/hooks/useCallback";
+import { useMemo } from "../../@lib/hooks/useMemo";
+import { useNotificationContext } from "../notification/NotificationProvider";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface UserContextType {
+  user: User | null;
+  login: (email: string, password: string) => void;
+  logout: () => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUserContext must be used within an AppProvider");
+  }
+  return context;
+};
+
+export const UserProvider = ({ children }: React.ReactNode) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const { addNotification } = useNotificationContext();
+
+  const login = useCallback((email: string) => {
+    setUser({ id: 1, name: "홍길동", email });
+    addNotification("성공적으로 로그인되었습니다", "success");
+  }, []);
+
+  const logout = useCallback(() => {
+    setUser(null);
+    addNotification("로그아웃되었습니다", "info");
+  }, []);
+
+  const userValue: UserContextType = useMemo(
+    () => ({ user, login, logout }),
+    [user, login, logout]
+  );
+
+  return (
+    <UserContext.Provider value={userValue}>{children}</UserContext.Provider>
+  );
+};
