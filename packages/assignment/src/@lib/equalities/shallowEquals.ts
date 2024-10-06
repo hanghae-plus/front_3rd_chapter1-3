e017513832fd23d73003ebba1398fe5b76c7b3d7
terry.hwang <terry.hwang@h2ohospitality.io>
2024-10-06 18:28:21 +0900

    shallowEquals 길이 비교 추가

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -3,6 +3,11 @@ export function shallowEquals(objA: any, objB: any): boolean {
   if (typeof objA !== typeof objB) return false;
 
   if (typeof objA === "object" && !!objA && !!objB) {
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) return false;
+
     return Object.keys(objA).every((key) => objA[key] === objB[key]);
   }
 
