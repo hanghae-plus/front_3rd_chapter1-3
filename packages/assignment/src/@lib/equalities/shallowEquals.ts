47e8635394080a5e27638a20ec64119520a16b32
ooyuo <doyounjeong0@gmail.com>
2024-10-06 18:39:13 +0900

    feat: 얕은 비교와 깊은 비교 함수 추가

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,4 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  return keysA.every((key) => objA[key] === objB[key]);
 }
