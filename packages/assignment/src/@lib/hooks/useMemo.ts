c64a170817c5bda4bdb1cf236bd206c982f6db6e
정원 <jwha94@gmail.com>
2024-10-07 01:05:39 +0900

    feat: useMemo 추가

--- packages/assignment/src/@lib/hooks/useMemo.ts
+++ packages/assignment/src/@lib/hooks/useMemo.ts
@@ -1,10 +1,20 @@
-import { DependencyList } from "react";
-import { shallowEquals } from "../equalities";
+import { DependencyList } from 'react';
+import { shallowEquals } from '../equalities';
+import { useRef } from './useRef';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function useMemo<T>(factory: () => T, deps: DependencyList, equals = shallowEquals): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+
+export function useMemo<T>(
+  factory: () => T,
+  deps: DependencyList,
+  equals = shallowEquals
+): T {
+  const prefRef = useRef<{ value: T; deps: DependencyList } | null>(null);
+
+  if (prefRef.current === null || !equals(deps, prefRef.current?.deps)) {
+    prefRef.current = { value: factory(), deps };
+  }
+
+  return prefRef.current.value;
 }
