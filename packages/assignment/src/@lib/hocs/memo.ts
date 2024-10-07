6d30cfde80c2b61aa289db3af7a3d187f4896392
백광현 <bgh9651@gmail.com>
2024-10-07 23:00:54 +0900

    feat: React.memo

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,6 +1,6 @@
-import { useRef } from "../hooks/useRef";
-import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { useRef } from '../hooks/useRef';
+import { shallowEquals } from '../equalities';
+import { ComponentType, FunctionComponent, createElement } from 'react';
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
@@ -8,16 +8,25 @@ export function memo<P extends object>(
   // @ts-ignore
   equals = shallowEquals
 ) {
-  // 1. 이전 props를 저장할 ref 생성
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const prevRef = useRef<P | null>(null);
   // 2. 메모이제이션된 컴포넌트 생성
+  const MemoizedComponent: FunctionComponent<P> = (newProps: P) => {
+    // 1. 이전 props를 저장할 ref 생성
+    const previousPropsRef = useRef<P | null>(null);
+    const previousRenderedElementRef = useRef<ReturnType<
+      typeof createElement
+    > | null>(null);
+    const isUpdate =
+      !previousPropsRef.current || !equals(previousPropsRef.current, newProps);
 
-  // 3. equals 함수를 사용하여 props 비교
-  const isChanged = !equals(prevRef.current, Component);
-  // 4. props가 변경된 경우에만 새로운 렌더링 수행
-  if (isChanged) {
-    return Component;
-  }
-  return Component;
+    // 3. equals 함수를 사용하여 props 비교
+    if (isUpdate) {
+      // // 4. props가 변경된 경우에만 새로운 렌더링 수행
+      previousPropsRef.current = newProps;
+      previousRenderedElementRef.current = createElement(Component, newProps);
+    }
+
+    return previousRenderedElementRef.current;
+  };
+
+  return MemoizedComponent;
 }
