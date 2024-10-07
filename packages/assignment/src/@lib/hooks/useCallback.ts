0b6c061fc49d403c2d223428327f3814dccf5adb
lsw <hkbvc1222@gmail.com>
2024-10-08 08:20:06 +0900

    feat: useRef, useMemo, useCallback 구현

--- packages/assignment/src/@lib/hooks/useCallback.ts
+++ packages/assignment/src/@lib/hooks/useCallback.ts
@@ -1,9 +1,7 @@
-import { DependencyList } from "react";
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-explicit-any
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { DependencyList } from 'react';
+import { useMemo } from './useMemo';
 export function useCallback<T extends (...args: any[]) => any>(factory: T, deps: DependencyList): T {
-  // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return ((...args) => factory(...args)) as T
+  return useMemo(() => factory, deps);
 }
