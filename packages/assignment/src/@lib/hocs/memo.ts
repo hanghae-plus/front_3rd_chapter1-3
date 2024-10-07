86aed0b7b44bb63f6c6ec4b395f5d816d00bc94f
Bong <borobong230@gmail.com>
2024-10-07 23:50:15 +0900

    :sparkles: [Feat] memo.ts 함수 구현

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,12 +1,26 @@
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, createElement } from "react";
+import { useRef } from "../hooks/useRef";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   equals = shallowEquals
 ) {
-  return Component;
+  const MemoizedComponent = (props: P) => {
+    const prevPropsRef = useRef<P | null>(null);
+
+    // props가 변경되었는지 확인
+    const propsChanged =
+      prevPropsRef.current === null || !equals(prevPropsRef.current, props);
+
+    // props가 변경되었으면 prevPropsRef를 업데이트
+    if (propsChanged) {
+      prevPropsRef.current = props;
+    }
+
+    // 변경된 경우에만 컴포넌트를 리렌더링
+    return propsChanged ? createElement(Component, props) : null;
+  };
+
+  return MemoizedComponent;
 }
