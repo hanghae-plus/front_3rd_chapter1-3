6f0cefb4803cd418b5006f3cd95579f0a9d738ee
dongjang <jdwls27@naver.com>
2024-10-07 13:47:30 +0900

    useCallback return 부분 eslint 비활성화 주석 추가

--- packages/assignment/src/@lib/hooks/useCallback.ts
+++ packages/assignment/src/@lib/hooks/useCallback.ts
@@ -6,5 +6,7 @@ import { useMemo } from "./useMemo";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-explicit-any
 export function useCallback<T extends (...args: any[]) => any>(factory: T, deps: DependencyList): T {
   // 직접 작성한 useMemo를 통해서 만들어보세요.
+  
+  /* eslint-disable-next-line react-hooks/exhaustive-deps */
   return useMemo(() => factory, deps);
 }
