94abd9326527debc943f29f30cff130f7eb8c5d0
KYW <kouym79@gmail.com>
2024-10-08 01:35:41 +0900

    [Add] shallowEquals 구현  - 1. 두 값이 정확히 같은지 확인 (참조가 같은 경우)
     - 2. param의 null 및 undefined 체크
    
     - 3. 둘 중 하나라도 객체가 아닌 경우 처리
    
     - 4. 객체의 키 개수가 다른 경우 처리
    
     - 5. 모든 키에 대해 얕은 비교 수행

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,4 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+
+  if(objA === objB)
+    return true;
+
+  if((objA ===null || objB === null) || (objA ===undefined || objB === undefined))
+    return false;
+
+  const keyA = Object.keys(objA);
+  const keyB = Object.keys(objB);
+  
+
+  if(typeof objA !== 'object' || typeof objB !== 'object')
+    return false;
+
+  if(keyA.length !== keyB.length)
+    return false;
+  
+  for (const key of keyA){
+    if(objA[key] !== objB[key])
+      return false;
+  }
+
+  return true;
 }
