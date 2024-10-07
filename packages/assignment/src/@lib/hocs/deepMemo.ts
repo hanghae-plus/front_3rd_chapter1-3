93c168a82c60e91bf1a6b27ce51a981a2be19cae
김서준 <gimseojun@gimseojun-ui-MacBookAir.local>
2024-10-07 23:24:09 +0900

    2주차 기본과제 진행중

--- packages/assignment/src/@lib/hocs/deepMemo.ts
+++ packages/assignment/src/@lib/hocs/deepMemo.ts
@@ -1,9 +1,12 @@
 import { deepEquals } from "../equalities";
-import { ComponentType } from "react";
-import { memo } from "./memo.ts";
+import { FunctionComponent } from "react";
+import { memo } from "./memo";
 
+// deepMemo HOC는 컴포넌트의 props를 깊은 비교하여 불필요한 리렌더링을 방지합니다.
 export function deepMemo<P extends object>(
-  Component: ComponentType<P>,
+  Component: FunctionComponent<P>
 ) {
+  // deepEquals 함수를 사용하여 props 비교
+  // 앞에서 만든 memo를 사용
   return memo(Component, deepEquals);
 }
--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,12 +1,21 @@
+import React, { useRef } from "react";
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
 
 export function memo<P extends object>(
-  Component: ComponentType<P>,
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  Component: React.ComponentType<P>,
   equals = shallowEquals
 ) {
-  return Component;
+  return function MemoizedComponent(props: P) {
+    // 1. 이전 props를 저장할 ref 생성
+    const prevPropsRef = useRef<P | null>(null);
+    const shouldRender = !prevPropsRef.current || !equals(prevPropsRef.current, props);
+
+    // 2. props가 변경된 경우에만 컴포넌트를 리렌더링하고, 이전 props를 업데이트
+    if (shouldRender) {
+      prevPropsRef.current = props;
+    }
+
+    // 3. 리렌더링 조건에 맞게 컴포넌트 반환
+    return shouldRender ? React.createElement(Component, props) : prevPropsRef.current;
+  };
 }
