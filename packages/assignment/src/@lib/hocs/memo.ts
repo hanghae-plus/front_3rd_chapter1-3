f569bbd66717d99a7591c9ed833d1b6619d8a4c1
yujin-sof <yujinsoft1111@gmail.com>
2024-10-07 19:06:34 +0900

    memo, useMemo 구현

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,5 +1,6 @@
+import React from "react";
 import { shallowEquals } from "../equalities";
-import { ComponentType, useRef, useState } from "react";
+import { ComponentType, useRef } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
@@ -10,27 +11,26 @@ export function memo<P extends object>(
 ) {
   // 2. 메모이제이션된 컴포넌트 생성
   return function memoizedComponent(props: P) {
-
       // 1. 이전 props를 저장할 ref 생성
       const prevPropsRef = useRef<P | null>(null);
       const shouldRender = useRef(true)
 
       // 3. equals 함수를 사용하여 props 비교
-      if (prevPropsRef !== null && equals(prevPropsRef.current, props)) {
+      if (prevPropsRef.current && equals(prevPropsRef.current, props)) {
         shouldRender.current = false;
       } else {
         // 4. props가 변경된 경우에만 새로운 렌더링 수행
         shouldRender.current = true;
       }
 
-      prevPropsRef.current = props;
-
       // 리렌더링 여부에 따라 컴포넌트를 렌더링
-      if (!shouldRender.current && prevPropsRef.current !== null) {
+      if (!shouldRender.current && prevPropsRef.current) {
         return null; // 리렌더링을 막음
       }
 
-      // return React.createElement(Component, props);
+      prevPropsRef.current = props;
+
+      return React.createElement(Component, props);
     }
   
 }
--- packages/assignment/src/@lib/hooks/useMemo.ts
+++ packages/assignment/src/@lib/hooks/useMemo.ts
@@ -1,10 +1,35 @@
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function useMemo<T>(factory: () => T, deps: DependencyList, equals = shallowEquals): T {
   // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+
+  // 1. 이전 의존성과 결과를 저장할 ref 생성
+  // usetMemo느ㄴ 인자로 factory 함수?와 deps를 받음. 추가로 얕은비교까지.
+  const memoizedValueRef = useRef<T | null>(null);
+  const prevDepsRef = useRef<any[] | null>(null);
+
+  // 2. 현재 의존성과 이전 의존성 비교
+  const hasChanged = () => {
+    if (!prevDepsRef.current) {
+      return true; // 처음 실행일 경우 변경된 것으로 간주
+    }
+
+    return deps.some((dep, i) => dep !== prevDepsRef.current![i]);
+  };
+
+  // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
+  if (hasChanged()) {
+    memoizedValueRef.current = factory();
+    prevDepsRef.current = [...deps];
+  }
+
+  // 4. 메모이제이션된 값 반환
+
+  // 구현을 완성해주세요.
+  return memoizedValueRef.current!;
 }
