30bf1975edf226ea8ddc211f36a7d4b1c7366d9a
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-08 01:31:21 +0900

    ♻️ refactor :: 얕은, 깊은 비교 구현 시 초기 불필요한 배열과 객체인 경우에 대한 조건 리팩토링

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,9 +1,5 @@
-interface AnyObject {
-  [key: string]: any;
-}
-
 export function deepEquals(objA: any, objB: any): boolean {
-  // 1. 기본 타입이거나 null인 경우 처리
+  // 1. 두 값이 정확히 같은지 확인 (참조가 같은 경우)
   if (objA === objB) {
     return true;
   }
@@ -13,42 +9,13 @@ export function deepEquals(objA: any, objB: any): boolean {
     return false;
   }
 
-  // 3. 배열인 경우
-  if (Array.isArray(objA) && Array.isArray(objB)) {
-    // 3-1. 배열의 길이가 같은지 확인 후, 각 요소에 대해 깊은 비교 수행
-    return deepCompareArrays(objA, objB);
-  }
-
-  // 4. 일반 객체인 경우
-  if (!Array.isArray(objA) && !Array.isArray(objB)) {
-    // 4-1. 객체의 키 개수가 같은지 확인 후, 각 키에 대해 깊은 비교 수행
-    return deepCompareObjects(objA, objB);
-  }
-
-  // 5. 타입이 일치하지 않는 경우 (한쪽은 배열이고 다른 한쪽은 객체)
-  return false;
-}
-
-function deepCompareArrays(arrayA: any[], arrayB: any[]): boolean {
-  if (arrayA.length !== arrayB.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayA.length; i++) {
-    if (!deepEquals(arrayA[i], arrayB[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function deepCompareObjects(objA: AnyObject, objB: AnyObject): boolean {
   const keysA = Object.keys(objA);
   const keysB = Object.keys(objB);
   if (keysA.length !== keysB.length) {
     return false;
   }
   for (const key of keysA) {
-    // 재귀적으로 각 속성에 대해 deepEquals 호출
+   // 재귀적으로 각 속성에 대해 deepEquals 호출
     if (!keysB.includes(key) || !deepEquals(objA[key], objB[key])) {
       return false;
     }
--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,7 +1,3 @@
-interface AnyObject {
-  [key: string]: any;
-}
-
 export function shallowEquals(objA: any, objB: any): boolean {
   // 1. 두 값이 정확히 같은지 확인 (참조가 같은 경우)
   if (objA === objB) {
@@ -13,33 +9,6 @@ export function shallowEquals(objA: any, objB: any): boolean {
     return false;
   }
 
-  // 배열인 경우
-  if (Array.isArray(objA) && Array.isArray(objB)) {
-    return shallowCompareArrays(objA, objB);
-  }
-
-  // 일반 객체인 경우
-  if (!Array.isArray(objA) && !Array.isArray(objB)) {
-    return shallowCompareObjects(objA, objB);
-  }
-
-  // 타입이 일치하지 않는 경우 (한쪽은 배열이고 다른 한쪽은 객체)
-  return false;
-}
-
-function shallowCompareArrays(arrayA: any[], arrayB: any[]): boolean {
-  if (arrayA.length !== arrayB.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayA.length; i++) {
-    if (arrayA[i] !== arrayB[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function shallowCompareObjects(objA: AnyObject, objB: AnyObject): boolean {
   const keysA = Object.keys(objA);
   const keysB = Object.keys(objB);
   // 3. 객체의 키 개수가 다른 경우 처리
