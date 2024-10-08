c4225fa2076600f0d33400bc2468820606a2ccff
KYW <kouym79@gmail.com>
2024-10-08 14:23:16 +0900

    [Add] deepEquals구현
    1. 기본 타입이거나 null인 경우 처리
    
    2. 둘 다 객체인 경우:
        - 배열인지 확인
        - 객체의 키 개수가 다른 경우 처리
        - 재귀적으로 각 속성에 대해 deepEquals 호출
        - hasOwnProperty를 활용해서 객체의 키값이 존재하는지 체크하는 경우도 좋을 것 같다.

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,28 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function deepEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+
+  if(objA===objB) return true;
+
+  if(typeof objA !== 'object' || typeof objB !== 'object')
+    return false;
+
+  if((objA === null || objA === undefined) && (objB === null || objB === undefined))  
+    return true;
+
+  const keyA = Object.keys(objA);
+  const keyB = Object.keys(objB);
+
+ 
+  if(keyA.length !== keyB.length) return false;
+    
+   
+
+  for(const key of keyA){
+    //!objB.hasOwnProperty(key) || 키 존재 여부도 체크하면 좋을것 같은 생각.
+    if( !deepEquals(objA[key],objB[key]))
+      return false;
+  }
+  
+
+  return true
 }
