f86077c125415684d3d04f0c1a48a2bf44e06c7b
forest-k <bstnqls@naver.com>
2024-10-09 02:44:47 +0900

    feat: memo 직접 구현하기

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,12 +1,26 @@
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, createElement, ReactElement } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   equals = shallowEquals
 ) {
-  return Component;
+  // 1. 클로저를 통해 prevProps와 memoizedResult 유지
+  let prevProps: P | null = null;
+  let memoizedResult: ReactElement | null = null;
+
+  // 2. 메모이제이션된 컴포넌트 반환
+  return (props: P) => {
+    // 3. 이전 props와 현재 props를 비교
+    if (!prevProps || !equals(prevProps, props)) {
+      // props가 변경된 경우에만 렌더링 수행
+      memoizedResult = createElement(Component, props);
+
+      // 이전 props를 현재 props로 갱신
+      prevProps = props;
+    }
+
+    // 메모이제이션된 결과 반환
+    return memoizedResult;
+  };
 }
