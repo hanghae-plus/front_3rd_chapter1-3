dac549c3b229f93f46544d3113b83258a1798ff5
이동아 <christar99@naver.com>
2024-10-08 18:03:58 +0900

    userContext - notification 추가

--- packages/assignment/src/@lib/context/user-context.tsx
+++ packages/assignment/src/@lib/context/user-context.tsx
@@ -1,4 +1,6 @@
 import { createContext, PropsWithChildren, useState } from "react";
+import { useContextHook } from "./useContextHook";
+import { NotificationContext } from "./notification-context";
 
 export interface UserType {
   id: number;
@@ -16,15 +18,19 @@ export const UserContext = createContext<UserContextType | null>(null);
 
 export const UserProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const [user, setUser] = useState<UserContextType["user"] | null>(null);
+  const { addNotification } = useContextHook({
+    context: NotificationContext,
+    name: "Notification",
+  });
 
   const login = (email: string) => {
     setUser({ id: 1, name: "홍길동", email });
-    // addNotification("성공적으로 로그인되었습니다", "success");
+    addNotification("성공적으로 로그인되었습니다", "success");
   };
 
   const logout = () => {
     setUser(null);
-    // addNotification("로그아웃되었습니다", "info");
+    addNotification("로그아웃되었습니다", "info");
   };
 
   return <UserContext.Provider value={{ user, login, logout }}>{children}</UserContext.Provider>;
