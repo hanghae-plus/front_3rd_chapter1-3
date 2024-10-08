57527246f033ba8e18b0644ce05d5d2c1dde0632
soyoonJ <sytkfkd96@gmail.com>
2024-10-09 00:07:16 +0900

    refactor: shallowEquals.ts, deepEquals.ts type 비교 추가 - type 비교에 undefined 포함되기 때문에 제외

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
-	if (objA === undefined || objA === null || typeof objA !== "object") return objA === objB;
+	if (typeof objA !== typeof objB) return false;
+	if (objA === null || typeof objA !== "object") return objA === objB;
 
 	const keysA = Object.keys(objA);
 	const keysB = Object.keys(objB);
