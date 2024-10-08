9d6db47b13d0b14e0ecbbf373d7299270f5c9a8a
hyeyum0312 <hyeyum0312@gmail.com>
2024-10-08 18:30:05 +0900

    비교함수 구현 - shallowEquals

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,4 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+    if (Object.is(objA, objB)) {
+        return true;
+    }
+
+    if (typeof objA !== "object" || objA === null || typeof objB !== "object" || objB === null) {
+        return false;
+    }
+
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+        return false;
+    }
+
+    for (const key of keysA) {
+        // objA에 있는 모든 키가 objB에도 존재하는지 확인
+        if (!keysB.includes(key) || !Object.is(objA[key], objB[key])) {
+            return false;
+        }
+    }
+
+    return true;
 }
