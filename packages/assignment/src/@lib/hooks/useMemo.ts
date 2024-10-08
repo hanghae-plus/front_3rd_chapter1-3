a26bf7ff8181e880a9d144ea99371f83496b6af1
codeeon <yeon.develop@gmail.com>
2024-10-09 00:56:18 +0900

    Fix: useMemo dependencies

--- packages/assignment/src/@lib/hooks/useMemo.ts
+++ packages/assignment/src/@lib/hooks/useMemo.ts
@@ -2,15 +2,11 @@ import { DependencyList } from 'react';
 import { shallowEquals } from '../equalities';
 import { useRef } from '.';
 
-export function useMemo<T>(
-  factory: () => T,
-  deps: DependencyList,
-  equals = shallowEquals
-): T {
+export function useMemo<T>(factory: () => T, deps: DependencyList, equals = shallowEquals): T {
   const ref = useRef<T | null>(null);
-  const dependencies = useRef<DependencyList>(deps);
+  const dependencies = useRef<DependencyList | null>(null);
 
-  if (!ref.current || !equals(dependencies.current, deps)) {
+  if (ref.current === null || !equals(dependencies.current, deps)) {
     ref.current = factory();
     dependencies.current = deps;
   }
