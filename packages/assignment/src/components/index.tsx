aaf0e9e0cd6c17dfba9f28dedd9ab45c749e90c2
forest-k <bstnqls@naver.com>
2024-10-10 01:12:10 +0900

    refactor: 컴포넌트 정의 폴더 분리

---
+++ packages/assignment/src/components/index.tsx
@@ -0,0 +1,6 @@
+import { Header } from "./Header";
+import { ComplexForm } from "./ComplexForm";
+import { ItemList } from "./ItemList";
+import { NotificationSystem } from "./NotificationSystem";
+
+export { Header, ComplexForm, ItemList, NotificationSystem };
