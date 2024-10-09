c68ad4a7c0751deffe61b2a5b89d21650970979f
hyeyum0312 <hyeyum0312@gmail.com>
2024-10-09 23:56:46 +0900

    refector: header,ItemList...컴포넌트 폴더로 분리

--- packages/assignment/src/contexts/AppProvider.tsx
+++ packages/assignment/src/contexts/AppProvider.tsx
@@ -1,6 +1,5 @@
 import { AuthProvider } from "./auth/AuthProvider";
 import { NotificationProvider } from "./notification/NotificationProvider";
-
 import { ThemeProvider } from "./theme/ThemeProvider";
 
 export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
