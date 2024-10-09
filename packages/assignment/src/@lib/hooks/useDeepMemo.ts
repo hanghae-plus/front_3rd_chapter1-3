0e9ab1f098a6562c66779f4f5ecd5cbca69608b4
seungwoo Kim <kyh196201@gmail.com>
2024-10-09 22:09:05 +0900

    feat: useDeepMemo 구현

--- packages/assignment/src/@lib/hooks/useDeepMemo.ts
+++ packages/assignment/src/@lib/hooks/useDeepMemo.ts
@@ -1,11 +1,16 @@
 import { DependencyList } from 'react';
-import { useMemo } from './useMemo';
 import { deepEquals } from '../equalities';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+import { useRef } from './useRef';
 
 export function useDeepMemo<T>(factory: () => T, deps: DependencyList): T {
-  // 직접 작성한 useMemo를 참고해서 만들어보세요.
-  return useMemo(factory, deps, deepEquals);
+  const prevDeps = useRef<DependencyList | null>(null);
+  const memoized = useRef<T | null>(null);
+
+  if (!prevDeps.current || !deepEquals(prevDeps.current, deps)) {
+    memoized.current = factory();
+  }
+
+  prevDeps.current = deps;
+
+  return memoized.current as T;
 }
