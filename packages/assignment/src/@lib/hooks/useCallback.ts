589301180bc4ad7786b9ff59da257b5b002d112d
Harry <harry@bzznbyd.com>
2024-10-09 21:33:07 +0900

    feat: useCallback 구현

--- packages/assignment/src/@lib/hooks/useCallback.ts
+++ packages/assignment/src/@lib/hooks/useCallback.ts
@@ -1,9 +1,15 @@
-import { DependencyList } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-explicit-any
+import { DependencyList } from "react";
+import { useMemo } from "./useMemo";
+
+/**
+ * @description useCallback 훅은 주어진 의존성 배열이 변경되지 않으면 동일한 함수를 반환합니다.
+ * @param factory - 메모이제이션할 함수
+ * @param deps - 의존성 배열
+ * @returns 메모이제이션된 콜백 함수 반환
+ */
 export function useCallback<T extends (...args: any[]) => any>(factory: T, deps: DependencyList): T {
-  // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return ((...args) => factory(...args)) as T
+  return useMemo(() => factory, deps); // useMemo를 사용하여 함수를 메모이제이션
 }
