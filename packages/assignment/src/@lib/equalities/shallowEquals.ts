ea997517e996e1b43f8a2c60c7da17c87fa12313
HaEun <rjadmszhd777@naver.com>
2024-10-09 13:55:22 +0900

    feat: shallowEquals.ts 얕은 비교을 수행하는 함수 구현 완료

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,4 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+  if (objA === objB)  {
+    return true;
+  }
+
+  if( typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if(keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for(const key of keysA) {
+    if(!Object.prototype.hasOwnProperty.call(objB, key) || objA[key] !== objB[key]) {
+      return false;
+    }
+  }
+
+
+  return true;
 }
+
