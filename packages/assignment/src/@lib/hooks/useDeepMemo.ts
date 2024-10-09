1ce66869e15505597b9a88a2e17bab63216fdce7
Harry <harry@bzznbyd.com>
2024-10-09 21:32:43 +0900

    feat: useMemo, useDeepMemo 구현

--- packages/assignment/src/@lib/hooks/useDeepMemo.ts
+++ packages/assignment/src/@lib/hooks/useDeepMemo.ts
@@ -1,11 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { DependencyList } from "react";
 import { useMemo } from "./useMemo";
 import { deepEquals } from "../equalities";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-
+/**
+ * @description 깊은 비교를 사용하여 메모이제이션된 값을 반환하는 훅입니다.
+ * @param factory - 메모이제이션할 값을 생성하는 함수
+ * @param deps - 의존성 배열 (deepEquals로 깊은 비교하여 변경 여부를 확인)
+ * @returns 메모이제이션된 값 반환
+ */
 export function useDeepMemo<T>(factory: () => T, deps: DependencyList): T {
-  // 직접 작성한 useMemo를 참고해서 만들어보세요.
-  return useMemo(factory, deps, deepEquals)
+  return useMemo(factory, deps, deepEquals);
 }
--- packages/assignment/src/@lib/hooks/useMemo.ts
+++ packages/assignment/src/@lib/hooks/useMemo.ts
@@ -1,10 +1,23 @@
-import { DependencyList } from "react";
+import { DependencyList, useRef } from "react";
 import { shallowEquals } from "../equalities";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/**
+ * @description useMemo 훅은 계산 비용이 높은 값을 메모이제이션하여, 의존성이 변경되지 않으면 이전에 계산된 값을 반환합니다.
+ * @param factory - 메모이제이션할 값을 계산하는 함수
+ * @param deps - 의존성 배열
+ * @param equals - 의존성 배열을 비교하는 함수 (기본값으로 shallowEquals 사용)
+ * @returns 메모이제이션된 값
+ */
 export function useMemo<T>(factory: () => T, deps: DependencyList, equals = shallowEquals): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  const previousDeps = useRef<DependencyList | null>(null); // 이전 의존성과 결과를 저장할 ref 생성
+  const memoizedValue = useRef<T | null>(null); // 처음엔 null로 초기화하고 의존성 변화에 따라 업데이트
+
+  const hasChanged = previousDeps.current === null || !equals(previousDeps.current, deps); // 의존성 배열이 변경되었는지 비교
+
+  if (hasChanged) { // 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
+    memoizedValue.current = factory();
+    previousDeps.current = deps;
+  }
+  
+  return memoizedValue.current as T; // 메모이제이션된 값 반환
 }
