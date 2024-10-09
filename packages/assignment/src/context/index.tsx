5816c7f441e8c0ac5c7f5761b2961faddf4edaa6
forest-k <bstnqls@naver.com>
2024-10-10 01:10:54 +0900

    refactor: 전역 상태 관리(Context, Provider) 폴더 기능별로 분리

---
+++ packages/assignment/src/context/index.tsx
@@ -0,0 +1,15 @@
+import { ThemeContext, ThemeProvider } from "../context/ThemeContext";
+import { UserContext, UserProvider } from "../context/UserContext";
+import {
+  NotificationContext,
+  NotificationProvider,
+} from "../context/NotificationContext";
+
+export {
+  ThemeContext,
+  ThemeProvider,
+  UserContext,
+  UserProvider,
+  NotificationContext,
+  NotificationProvider,
+};
