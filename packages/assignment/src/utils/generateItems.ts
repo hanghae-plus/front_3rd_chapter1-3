26632c9858c7cc9b0207f68344a9a8ba798a2b2f
forest-k <bstnqls@naver.com>
2024-10-10 01:16:39 +0900

    refactor:  유틸리티 함수 폴더 분리

---
+++ packages/assignment/src/utils/generateItems.ts
@@ -0,0 +1,9 @@
+export const generateItems = (count: number) => {
+  const categories = ["전자기기", "의류", "도서", "식품"];
+  return Array.from({ length: count }, (_, index) => ({
+    id: index,
+    name: `상품 ${index}`,
+    category: categories[Math.floor(Math.random() * categories.length)],
+    price: Math.floor(Math.random() * 100000) + 1000,
+  }));
+};
---
+++ packages/assignment/src/utils/index.ts
@@ -0,0 +1,4 @@
+import { generateItems } from "./generateItems";
+import { renderLog } from "./renderLog";
+
+export { generateItems, renderLog };
