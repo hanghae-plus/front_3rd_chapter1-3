63132a3452bf3e85ec786166b787c3fc55850a47
temeraire97 <hyensooyoon@gmail.com>
2024-10-10 02:21:38 +0900

    FIX: console.log 삭제

--- packages/assignment/src/@lib/hooks/useRef.ts
+++ packages/assignment/src/@lib/hooks/useRef.ts
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 export function useRef<T>(initialValue: T): { current: T } {
-  console.log('useRef.initialValue:', initialValue);
   // React의 useState를 이용해서 만들어보세요.
   const [ref] = useState<{ current: T }>({ current: initialValue });
   return ref;
