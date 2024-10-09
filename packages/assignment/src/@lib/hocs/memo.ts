1ea2ad217fcc39aefff71bd3fbca63b15d0cd5df
HaEun <rjadmszhd777@naver.com>
2024-10-09 13:59:06 +0900

    feat: memo.ts 컴포넌트의 props를 얕은 비교해서 불필요한 랜더링 방지하는 함수 구현

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,3 +1,4 @@
+import React from 'react';
 import { shallowEquals } from "../equalities";
 import { ComponentType } from "react";
 
@@ -8,5 +9,21 @@ export function memo<P extends object>(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   equals = shallowEquals
 ) {
-  return Component;
+
+  let prevProps: P | null = null;
+  let prevResult: React.ReactElement|null = null;
+
+  const MemoizedComponent = (props:P) => {
+    if(prevProps === null || !equals(prevProps, props)) {
+      prevProps = props;
+      prevResult = React.createElement(Component, props);
+  }
+return prevResult;
+  }
+  
+  MemoizedComponent.displayName = `Memo(${Component.displayName || Component.name|| 'Component'})`;
+
+
+  return MemoizedComponent;
 }
+
