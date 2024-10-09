82a7a6c40887f2f011c75b85176a5284f32e7e64
김진현 <jinhyeon0810@gmail.com>
2024-10-10 00:11:08 +0900

    refactor Notification 기능 Context API구현

---
+++ packages/assignment/src/provider/notification/NotificationContext.ts
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+import { Notification } from "../../types";
+
+interface NotificationContextType {
+  notifications: Notification[];
+  addNotification: (message: string, type: Notification["type"]) => void;
+  removeNotification: (id: number) => void;
+}
+
+const NotificationContext = createContext<NotificationContextType | undefined>(
+  undefined
+);
+
+export default NotificationContext;
