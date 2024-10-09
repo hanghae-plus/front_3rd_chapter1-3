42aa47f63e6ebc828a34cd7417b8f5e963367754
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-10-09 21:23:10 +0900

    deepEquals 구현

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -3,7 +3,6 @@ export function shallowEquals(objA: any, objB: any): boolean {
   //비교함수 구현하기 두 값의 얕은 비교 수행
   if (objA === objB) return true;
 
-  // objA와 objB의 타입이 다르면 바로 false 반환
   if (
     typeof objA !== "object" ||
     typeof objB !== "object" ||
