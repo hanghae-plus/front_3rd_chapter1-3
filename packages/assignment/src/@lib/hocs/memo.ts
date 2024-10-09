408ebb8482712426778fbdb9fd3ee2c8df501bb1
oStone <lws2269@kakao.com>
2024-10-09 21:32:20 +0900

    refactor: memo.ts 상수 통합

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -4,6 +4,11 @@ import { ComponentType, createElement, ReactElement, useRef } from 'react';
 // ComponentType<MyProps>
 // -> const MyComponent: React.FC<MyProps> = (props) => { ... }
 
+type PropsType<T> = {
+  props: T;
+  result: ReactElement;
+};
+
 export function memo<P extends object>(Component: ComponentType<P>, equals = shallowEquals) {
   // 1. 이전 props를 저장할 ref 생성
   // 2. 메모이제이션된 컴포넌트 생성
@@ -11,16 +16,17 @@ export function memo<P extends object>(Component: ComponentType<P>, equals = sha
   // 4. props가 변경된 경우에만 새로운 렌더링 수행
   const MemoizedComponent = (props: P) => {
     // -> 1
-    const prevProps = useRef<P | null>(null);
-    const memoizedResult = useRef<ReactElement | null>(null);
+    const prevProps = useRef<PropsType<P> | null>(null);
 
     // 이전 상태 값이 없거나, 비교함수를 통해 다른 값일때 값 변경 -> 3,4
-    if (prevProps.current === null || !equals(prevProps.current, props)) {
+    if (prevProps.current === null || !equals(prevProps.current.props, props)) {
       // tsx라 <Component .../> 대신 createElement
-      memoizedResult.current = createElement(Component, props);
-      prevProps.current = props;
+      prevProps.current = {
+        result: createElement(Component, props),
+        props,
+      };
     }
-    return memoizedResult.current;
+    return prevProps.current.result;
   };
   return MemoizedComponent;
 }
