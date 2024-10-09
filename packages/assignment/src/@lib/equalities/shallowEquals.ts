f7e346e3cc2eabac7dc2b247bf8da2d00dc07354
codeeon <yeon.develop@gmail.com>
2024-10-09 11:55:58 +0900

    Feat: deepEquals

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function shallowEquals(objA: any, objB: any): boolean {
+export function shallowEquals(objA: any, objB: any, deepCompare: boolean = false): boolean {
   if (objA === objB) {
     return true;
   }
@@ -16,6 +16,10 @@ export function shallowEquals(objA: any, objB: any): boolean {
 
   if (keysA.length !== keysB.length) {
     return false;
+  }
+
+  if (deepCompare) {
+    return keysA.every((key) => shallowEquals(objA[key], objB[key], true));
   }
 
   return keysA.every((key) => objA[key] === objB[key]);
