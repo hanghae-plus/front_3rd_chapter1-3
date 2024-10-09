4ef68dc39d39915b27663d2de0db97652d7b1c1f
JaeWoongHwang <jaiuong@naver.com>
2024-10-09 17:16:43 +0900

    FIX : prettier 설정 및 기본과제 완료

--- packages/assignment/src/@lib/hooks/useRef.ts
+++ packages/assignment/src/@lib/hooks/useRef.ts
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState } from 'react';
 // <T> 뭘까? 타입이 무엇이든 가능하다는 뜻입니다.
 // use
 export function useRef<T>(initialValue: T): { current: T } {
   // React의 useState를 이용해서 만들어보세요.
-  const [ref, setRef] = useState({ current: initialValue });
+  const [ref] = useState({ current: initialValue });
   return ref;
 }
