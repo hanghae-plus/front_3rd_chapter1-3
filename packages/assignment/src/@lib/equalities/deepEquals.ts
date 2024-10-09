c6dcebe89ab8ec65e8b3ef12fa6cc74a38bd3f27
sanghyuk-2i <desummit1@gmail.com>
2024-10-09 12:54:16 +0900

    Feat: 기본과제 구현 완료
    - equals 및 hooks 구현 완료

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,41 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function deepEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+  const hasPrimitiveValue =
+    typeof objA !== 'object' ||
+    objA === null ||
+    typeof objB !== 'object' ||
+    objB === null;
+
+  if (hasPrimitiveValue) {
+    return objA === objB;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    for (let i = 0; i < objA.length; i++) {
+      if (!deepEquals(objA[i], objB[i])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  const keysOfA = Object.keys(objA);
+  const keysOfB = Object.keys(objB);
+
+  if (keysOfA.length !== keysOfB.length) {
+    return false;
+  }
+
+  for (const key of keysOfA) {
+    if (!deepEquals(objA[key], objB[key])) {
+      return false;
+    }
+  }
+
+  return true;
 }
