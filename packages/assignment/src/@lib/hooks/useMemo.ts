6467e5ec97adf9f184c6872c9592b55d290054bb
남균 <gyuni9604@naver.com>
2024-10-10 21:28:38 +0900

    ♻️ refactor : useMemo 리펙토링 뎁스의 기본값을 undefined 로 변경

--- packages/assignment/src/@lib/hooks/useMemo.ts
+++ packages/assignment/src/@lib/hooks/useMemo.ts
@@ -4,22 +4,18 @@ import { shallowEquals } from "../equalities"
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function useMemo<T>(
-  factory: () => T,
-  deps: DependencyList,
-  equals = shallowEquals
-): T {
+export function useMemo<T>(factory: () => T, deps: DependencyList, equals = shallowEquals): T {
   // 직접 작성한 useRef를 통해서 만들어보세요.
 
   const ref = useRef<{
-    deps: DependencyList | []
+    deps: DependencyList | undefined
     value: T | undefined
-  }>({ deps: [], value: undefined })
+  }>({ deps: undefined, value: undefined })
 
   // ref deps가 없거나 , deps가 ref deps와 다르다면
-  if (ref.current.deps.length === 0 || !equals(deps, ref.current.deps)) {
-    ref.current.deps = deps
+  if (!ref.current.deps || !equals(ref.current.deps, deps)) {
     ref.current.value = factory()
+    ref.current.deps = deps
   }
 
   return ref.current.value as T
