df98305a0aa92e07d67a35955a8f2379856084e4
iammiori <immioriii@gmail.com>
2024-10-10 21:54:27 +0900

    feat: add custom memo hoc

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,12 +1,24 @@
+import { ComponentType, createElement, ReactNode } from 'react';
 import { shallowEquals } from '../equalities';
-import { ComponentType } from 'react';
+import { useRef } from '../hooks';
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   equals = shallowEquals
 ) {
-  return Component;
+  return function MemoisedComponent(props: P) {
+    const prevPropsRef = useRef<P | null>(null);
+
+    const prevResultRef = useRef<ReactNode>(null);
+
+    if (!prevPropsRef.current || !equals(prevPropsRef.current, props)) {
+      prevResultRef.current = createElement(Component, props);
+      prevPropsRef.current = { ...props };
+    }
+
+    return prevResultRef.current;
+  };
 }
