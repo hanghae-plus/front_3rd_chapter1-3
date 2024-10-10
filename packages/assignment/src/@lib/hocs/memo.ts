f6a1de2ef711632d473d75a72b632a68e425ca13
LeeJaein <devxoox1106@gmail.com>
2024-10-10 12:13:26 +0900

    useMemo,useRef

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,5 +1,5 @@
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import React, { ComponentType , useRef } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
@@ -8,5 +8,15 @@ export function memo<P extends object>(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   equals = shallowEquals
 ) {
+  // 1. 이전 props를 저장할 ref 생성
+  const prevPropsRef = useRef();
+
+  // 2. 메모이제이션된 컴포넌트 생성
+  
+
+  // 3. equals 함수를 사용하여 props 비교
+
+  // 4. props가 변경된 경우에만 새로운 렌더링 수행
+
   return Component;
 }
