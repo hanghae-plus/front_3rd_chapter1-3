8fac54a97cdd590681148586530954dece1bebbe
iammiori <immioriii@gmail.com>
2024-10-10 21:09:02 +0900

    feat: implement deepEquals function

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,37 @@
+import { has, isArray, isNull, isObject, keys } from '../lodash';
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function deepEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+export function deepEquals(valueA: any, valueB: any): boolean {
+  if (valueA === valueB) {
+    return true;
+  }
+
+  if (isNull(valueA) || isNull(valueB)) {
+    return false;
+  }
+
+  if (isArray(valueA) && isArray(valueB)) {
+    return (
+      valueA.length === valueB.length &&
+      valueA.every((item, index) => deepEquals(item, valueB[index]))
+    );
+  }
+
+  if (isArray(valueA) || isArray(valueB)) {
+    return false;
+  }
+
+  if (!isObject(valueA) || !isObject(valueB)) {
+    return false;
+  }
+
+  const keysA = keys(valueA);
+  const keysB = keys(valueB);
+
+  return (
+    keysA.length === keysB.length &&
+    keysA.every(
+      (key) => has(valueB, key) && deepEquals(valueA[key], valueB[key])
+    )
+  );
 }
