66d0a84c4e7f52d546e8be3a0390b1848184f32d
doitidey <evxrafter@gmail.com>
2024-10-11 02:19:11 +0900

    Feature: memo 구현

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,5 +1,5 @@
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import React, { ComponentType } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
@@ -8,5 +8,24 @@ export function memo<P extends object>(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   equals = shallowEquals
 ) {
-  return Component;
+  // 1. 이전 props를 저장할 ref 생성
+
+  // 2. 메모이제이션된 컴포넌트 생성
+
+  // 3. equals 함수를 사용하여 props 비교
+
+  // 4. props가 변경된 경우에만 새로운 렌더링 수행
+
+  let prevProps: P | undefined = undefined;
+  let memorizedResult: React.ReactElement | undefined = undefined;
+
+  const memorizedComponent = (props: P) => {
+    if (prevProps === undefined || !equals(prevProps, props)) {
+      memorizedResult = React.createElement(Component, props);
+    }
+    prevProps = props;
+    return memorizedResult;
+  };
+
+  return memorizedComponent;
 }
