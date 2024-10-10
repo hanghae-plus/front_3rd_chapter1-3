f66ee2ce751d2dc3b2714bca122a32399a1a07ae
안기환 <ahngh0113@naver.com>
2024-10-10 15:28:47 +0900

    @lib/equalities(shallowEquals, deepEquals) 구현

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,48 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export function deepEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+  if (Object.is(objA, objB)) {
+    return true;
+  }
+
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  const hasOwnProp = Object.prototype.hasOwnProperty;
+
+  for (let i = 0; i < keysA.length; i++) {
+    const currentKey = keysA[i];
+
+    if (!hasOwnProp.call(objB, currentKey)) {
+      return false;
+    }
+
+    const valueA = objA[currentKey];
+    const valueB = objB[currentKey];
+
+    const isObjectA = typeof valueA === "object" && valueA !== null;
+    const isObjectB = typeof valueB === "object" && valueB !== null;
+
+    if (isObjectA && isObjectB) {
+      if (!deepEquals(valueA, valueB)) {
+        return false;
+      }
+    } else if (!Object.is(valueA, valueB)) {
+      return false;
+    }
+  }
+
+  return true;
 }
