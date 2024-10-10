08adef6ef0d4eaaffb548f77adbf11245864f992
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-10-10 09:29:47 +0900

    useRef 테스트에러 수정

--- packages/assignment/src/@lib/hooks/useRef.ts
+++ packages/assignment/src/@lib/hooks/useRef.ts
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 export function useRef<T>(initialValue: T): { current: T } {
   // React의 useState를 이용해서 만들어보세요.
 
-  const ref = { current: initialValue };
+  const [ref] = useState<{ current: T }>({ current: initialValue });
 
   return ref;
 }
