fd2ca8bc7803eb3efae4312ede4f28ad27ee9dd1
jhkim <jhkim@enterprise1.co.kr>
2024-10-10 16:59:14 +0900

    fix: shallowEquals, deepEquals

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -2,30 +2,20 @@
 // deepEquals 함수는 두 값의 깊은 비교를 수행합니다.
 export function deepEquals(objA: any, objB: any): boolean {
   // 1. 기본 타입이거나 null인경우 처리
-  if (objA === objB) {
-    return true;
-  }
+  if (objA === objB) return true;
+
+  // 2. 둘 다 객체인 경우:
   if (
     typeof objA !== "object" ||
     objA === null ||
     typeof objB !== "object" ||
     objB === null
-  ) {
+  )
     return false;
-  }
-  // 2. 둘 다 객체인 경우:
+
   // - 배열인지 확인
-  if (Array.isArray(objA) && Array.isArray(objB)) {
-    if (objA.length !== objB.length) {
-      return false;
-    }
-    for (let i = 0; i < objA.length; i++) {
-      if (!deepEquals(objA[i], objB[i])) {
-        return false;
-      }
-    }
-    return true;
-  }
+  if (Array.isArray(objA) !== Array.isArray(objB)) return false;
+
   // - 객체의 키 개수가 다른 경우 처리
   const keysA = Object.keys(objA);
   const keysB = Object.keys(objB);
