2238955bc9d1e392161c4473b80a0cadf23a70dc
Bong <borobong230@gmail.com>
2024-10-10 21:59:32 +0900

    :recycle: [Refactor] useCallback, useDeepMemo 경고문구 제거(eslinst 규칙 무시하는 주석 추가...) #main

--- packages/assignment/src/@lib/hooks/useCallback.ts
+++ packages/assignment/src/@lib/hooks/useCallback.ts
@@ -8,5 +8,6 @@ export function useCallback<T extends (...args: any[]) => any>(
   deps: DependencyList
 ): T {
   // @useMemo.ts 함수를 사용하여 메모이제이션을 적용합니다.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   return useMemo(() => factory, deps);
 }
