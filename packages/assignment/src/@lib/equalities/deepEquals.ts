9c9fce968379a9ffab317f59ec438c3002939ac4
Kang Seokhyun <yoo2yoo2yoo2@gmail.com>
2024-10-10 22:19:24 +0900

    🚚commonUtils -> utils

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,4 @@
-import { compareObjects } from "./commonUtils";
+import { compareObjects } from "./utils";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function deepEquals(objA: any, objB: any): boolean {
