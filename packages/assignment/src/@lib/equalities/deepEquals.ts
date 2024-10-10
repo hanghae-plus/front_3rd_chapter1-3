950a5321111377fadece95e176c1cd6b05076e1f
seriparkdev <parkseridev@gmail.com>
2024-10-10 15:06:14 +0900

    feat: 깊은 비교를 위한 deepEquals 함수 구현

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export function deepEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+  // 1. 기본 타입이거나 null인 경우 처리
+  if (objA === objB) return true;
+
+  if (
+    typeof objA !== "object" ||
+    typeof objB !== "object" ||
+    objA === null ||
+    objB === null
+  )
+    return false;
+
+  // 2. 둘 다 객체인 경우:
+  //    - 배열인지 확인
+  //    - 객체의 키 개수가 다른 경우 처리
+  //    - 재귀적으로 각 속성에 대해 deepEquals 호출
+  const objAKeys = Object.keys(objA);
+  const objBKeys = Object.keys(objB);
+
+  if (objAKeys.length !== objBKeys.length) return false;
+
+  return objAKeys.every(
+    (aKey) => objB.hasOwnProperty(aKey) && deepEquals(objA[aKey], objB[aKey])
+  );
 }
