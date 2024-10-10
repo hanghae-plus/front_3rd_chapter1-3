7a881702fc69678652f78c482d621142bf3c3f34
RyuSimyeong <rsm0503@gmail.com>
2024-10-11 00:11:13 +0900

    feat: useRef 이용 해서 useMemo 구현

--- packages/assignment/src/@lib/hooks/useMemo.ts
+++ packages/assignment/src/@lib/hooks/useMemo.ts
@@ -11,20 +11,17 @@ export function useMemo<T>(
   deps: DependencyList,
   equals = shallowEquals
 ): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요! 이게 제일 중요합니다.
   // 1. 이전 의존성과 결과를 저장할 ref 생성
-  const prevDepsRef = useRef<{ deps: DependencyList; value: T } | undefined>(
-    undefined
-  );
+  const prevResultRef = useRef<T | null>(null);
+  const prevDepsRef = useRef<DependencyList | null>(null);
 
   // 2. 현재 의존성과 이전 의존성 비교
-  if (!prevDepsRef.current || !equals(prevDepsRef.current.deps, deps)) {
-    // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
-    const newValue = factory();
-    prevDepsRef.current = { deps, value: newValue };
-    return newValue;
+  // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
+  if (prevResultRef.current === null || !equals(prevDepsRef.current, deps)) {
+    prevDepsRef.current = deps;
+    prevResultRef.current = factory();
   }
 
   // 4. 메모이제이션된 값 반환
-  return prevDepsRef.current.value;
+  return prevResultRef.current;
 }
