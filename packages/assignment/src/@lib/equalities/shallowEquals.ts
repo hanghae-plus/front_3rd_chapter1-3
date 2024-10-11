4f7030839e22001a9743ad21c91f86751ded4112
soyoonJ <sytkfkd96@gmail.com>
2024-10-12 01:43:18 +0900

    fix: equalities null 확인 조건 복구

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
 	if (Object.is(objA, objB)) return true;
-	if (typeof objA !== "object" || typeof objB !== "object") return false;
+	if (objA === null || objB === null || typeof objA !== "object" || typeof objB !== "object")
+		return false;
 
 	const keysA = Object.keys(objA);
 	const keysB = Object.keys(objB);
