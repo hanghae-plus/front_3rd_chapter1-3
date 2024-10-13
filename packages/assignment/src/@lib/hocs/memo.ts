e4c06da9125be0243e06bd54d6cecf2b03bf13dc
lunashp <hijnshp@gmail.com>
2024-10-13 16:17:24 +0900

    FIX: (!isEqual) > (!isEqual())

--- packages/assignment/src/@lib/hocs/memo.ts
+++ packages/assignment/src/@lib/hocs/memo.ts
@@ -1,10 +1,10 @@
 import { shallowEquals } from "../equalities";
 import { ComponentType, createElement } from "react";
-import { useRef } from "../hooks/useRef";
-import { useCallback } from "../hooks";
+import { useCallback, useRef } from "../hooks";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
+
   equals = shallowEquals
 ) {
   // 메모이제이션된 컴포넌트 생성
@@ -12,18 +12,17 @@ export function memo<P extends object>(
     // 이전 props를 저장할 ref 생성
     const prevPropsRef = useRef<P | null>(null);
 
-    // equals 함수를 사용하여 props 비교
+    // useCallback을 사용하여 equals 함수 메모이제이션
     // useCallback으로 isEqual 함수를 메모이제이션하여, props가 변경될 때마다 불필요한 함수 재생성을 방지
     const isEqual = useCallback(() => {
+      // 이전 props와 현재 props를 비교
       return prevPropsRef.current && equals(prevPropsRef.current, props);
     }, [props]);
 
-    // props가 변경된 경우에만 새로운 렌더링 수행
-    if (!isEqual) {
-      prevPropsRef.current = props; // 이전 props 업데이트
-      return createElement(Component, props); // React.createElement 사용
+    if (!isEqual()) {
+      prevPropsRef.current = props; // props가 변경되었을 때 업데이트
+      return createElement(Component, props); // props가 변경된 경우에만 컴포넌트 렌더링
     }
-
     return null; // props가 같으면 렌더링하지 않음
   }
 
