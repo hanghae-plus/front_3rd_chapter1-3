f5037a1217118b0ee8db2161349fe7c4b6e8d6d8
lunashp <hijnshp@gmail.com>
2024-10-13 14:52:47 +0900

    DOCS: shallowEquals 에서 배열로직을 구현하지 않는 이유 주석 추가

--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,6 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
   // 1. 두 값이 정확히 같은지 확인 (참조가 같은 경우)
+
+  // 이 코드에서 objA와 objB가 같은 참조를 가리키면(같은 배열이거나 같은 객체일 경우), 바로 true를 반환.
+  // 따라서 별도의 배열 참조 비교 로직을 추가할 필요없음
   if (objA === objB) {
     return true;
   }
