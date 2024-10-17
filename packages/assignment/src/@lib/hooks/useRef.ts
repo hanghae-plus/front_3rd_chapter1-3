488e9e8ae69738cb3c06d18a259451fbe293aeeb
yujin-sof <yujinsoft1111@gmail.com>
2024-10-18 06:23:16 +0900

    심화과제 진행중

--- packages/assignment/src/@lib/hooks/useRef.ts
+++ packages/assignment/src/@lib/hooks/useRef.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export function useRef<T>(initialValue: T): { current: T } {
   // React의 useState를 이용해서 만들어보세요.
-  const [ref, unused] = useState({ current: initialValue });
+  const [ref] = useState({ current: initialValue });
 
   return ref; 
 }
